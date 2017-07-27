24c24f1792f0bcb5605271a9ee0caba0d54f8d41
jochen <942814208@qq.com>
2017-07-27 09:45:17 +0800

    递归爬取wiki百度页面链接

--- spider2.py
+++ spider2.py
@@ -1,4 +1,6 @@
-#coding=utf-8
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
@@ -8,13 +10,19 @@ def getLinks(pageUrl):
     global pages
     html = urlopen("https://en.wikipedia.org"+pageUrl)
     bsObj = BeautifulSoup(html,"html.parser")
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id="mw-content-text").findAll("p")[0])
+        print(bsObj.find(id="ca-edit").find("span").find("a").attrs['href'])
+    except AttributeError:
+        print("Missing some attributes")
     for link in bsObj.findAll("a",href=re.compile("^(/wiki/)")):
         if 'href'in link.attrs:
             if link.attrs['href'] not in pages:
             #遇到了新页面
                 newPage = link.attrs['href']
-                print (newPage)
+                print ("------------\n"+newPage)
                 pages.add(newPage)
                 getLinks(newPage)
-getLinks('')
+getLinks("")
 
