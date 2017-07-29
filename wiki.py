e895a3771da8168b5d33f3c1848e7124bb07007c
jochen <942814208@qq.com>
2017-07-29 21:50:43 +0800

    采集wiki百科python历史编辑页面匿名编辑者ip地址对应物理地址

--- wiki.py
+++ wiki.py
@@ -11,9 +11,6 @@ import random
 import re
 import json
 
-
-
-
 random.seed(datetime.datetime.now())                         #生成一个以当前系统时间为随机种子的随机系统参数
 def getLinks(articleUrl):                                   #建立一个getLinks函数。作用：把以en.wikipedia.org域名开头的域名用beaurifulsoup解析，并通过正则表达式等条件筛选并返回该页面中所有指向其他词条的链接。
     html = urlopen("http://en.wikipedia.org"+articleUrl)     #当前python词条历史编辑页面含有词条链接和其他链接（ps：维基百科的每个页面都充满了侧边栏、页眉、页脚链接，以及连接到分类页面、对话页面和其他不包含词条的页面的链接）
@@ -28,13 +25,13 @@ def getHistoryIPs(pageUrl):   #编辑历史页面URL链接格式:  https://en.wi
     bsObj = BeautifulSoup(html,"html.parser")
     #找出class属性是“mw——anonuserlink”的链接
     #它们用IP地址代替用户名
-    ipAddresses = bsObj.findAll("a",{"class":"mw-anonuserlink"})
+    ipAddresses = bsObj.findAll("a",{"class":"mw-anonuserlink"})         #以后mw后面一定要看清楚是下划线还是减号啊啊啊啊啊啊！！！！！！！
     addressList = set()
     for ipAddress in ipAddresses:
         addressList.add(ipAddress.get_text())
     return addressList
 
-def getCountry(ipAddress):
+def getCountry(ipAddress):       #调用API，获取ip地址对应的地理位置
     try:
         response = urlopen("http://freegeoip.net/json/"+ipAddress).read().decode('utf_8')
     except HTTPError:
@@ -52,7 +49,16 @@ while(len(links) > 0):
             country = getCountry(historyIP)
             if country is not None:
                 print(historyIP+"is from"+country)
-    newLink = links[random.randit(0,len(links)-1)].attrs["href"]
-    links = getLinks(newLink)
+    newLink = links[random.randit(0,len(links)-1)].attrs["href"]   #random.randit(a,b)  a到b之前产生一个随机数（包含a，）
+    links = getLinks(newLink)                                      #tag.attrs  取tag全部属性
 
-
+'''#!/usr/bin/Python       #这段代码为验证这个函数getHistoryIPs(pageUrl)的输结果
+# -*- coding: utf-8 -*-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+historyUrl = "https://en.wikipedia.org/w/index.php?title=Python_(programming_language)&action=history"
+html = urlopen(historyUrl)
+bsObj = BeautifulSoup(html, "html.parser")
+ipAddresses = bsObj.findAll("a", {"class": "mw-anonuserlink"})
+for ipAddress in ipAddresses:
+    print(ipAddress.get_text())'''
